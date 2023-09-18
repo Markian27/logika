2df3fec4f2e76f6578c51c998e9dde3710a39885
Markian27 <144822485+Markian27@users.noreply.github.com>
2023-09-18 20:40:57 +0300

    Add files via upload

---
+++ u3_2.py
@@ -0,0 +1,37 @@
+class Student():
+    def __init__(self, surname, name, qreat):
+        self.surname = surname
+        self.name = name
+        self.qreat = qreat
+
+student = []
+
+with open("sts.txt", encoding="utf-8") as file:
+    for line in file:
+        data = line.split(" ")
+        jd = Student(data[0], data[1], int(data[2]))
+        student.append(jd)
+        pt = len(student)
+suma = 0
+ss = 0
+for s in student:
+    if s.qreat == 5:
+        print(s.surname, s.name, 'Оцінка:', s.qreat)
+        suma += 5
+        ss += 1
+    if s.qreat == 4:
+        suma += 4
+        ss += 1
+    if s.qreat == 3:
+        suma += 3
+        ss += 1
+    if s.qreat == 2:
+        suma += 2
+        ss += 1
+    medium = suma / ss
+
+print('Загальна кількість балів:', suma)
+print('Середнє арифметичне:', medium)
+print('Кількість студентів:', pt)
+
+
