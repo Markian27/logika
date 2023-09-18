b4182c911de5ce658d83e3d67eec9429b315f34f
Markian27 <144822485+Markian27@users.noreply.github.com>
2023-09-18 20:51:35 +0300

    Update u3_2.py

--- logika/m3/u3_2.py
+++ logika/m3/u3_2.py
@@ -11,9 +11,27 @@ with open("sts.txt", encoding="utf-8") as file:
         data = line.split(" ")
         jd = Student(data[0], data[1], int(data[2]))
         student.append(jd)
-
+        pt = len(student)
+suma = 0
+ss = 0
 for s in student:
     if s.qreat == 5:
         print(s.surname, s.name, 'Оцінка:', s.qreat)
+        suma += 5
+        ss += 1
+    if s.qreat == 4:
+        suma += 4
+        ss += 1
+    if s.qreat == 3:
+        suma += 3
+        ss += 1
+    if s.qreat == 2:
+        suma += 2
+        ss += 1
+    medium = suma / ss
+
+print('Загальна кількість балів:', suma)
+print('Середнє арифметичне:', medium)
+print('Кількість студентів:', pt)
 
 
