16e8285ff529a0c471a452d3f28c4adfd4b3323f
Markian27 <144822485+Markian27@users.noreply.github.com>
2023-09-22 20:59:32 +0300

    Update main.py

--- logika/m3/smart_notes/main.py
+++ logika/m3/smart_notes/main.py
@@ -12,12 +12,16 @@ window = QWidget()
 line1 = QVBoxLayout()
 line2 = QVBoxLayout()
 
-line_x1 = QHBoxLayout()
-line_x2 = QHBoxLayout()
 layout_notes = QHBoxLayout()
 
 layout_notes.addLayout(line1, stretch=2)
 layout_notes.addLayout(line2, stretch=1)
+
+line_x1 = QHBoxLayout()
+line_x2 = QHBoxLayout()
+line2.addLayout(line_x1)
+line2.addLayout(line_x2)
+
 
 file_text = QTextEdit()
 line1.addWidget(file_text)
@@ -28,16 +32,27 @@ line2.addWidget(id_notes)
 lst_notes = QListWidget()
 line2.addWidget(lst_notes)
 
-lmt_notes = QLabel('Список тегів ')
-
-
 
 btn_create_notes = QPushButton('Створити замітку')
+line_x1.addWidget(btn_create_notes)
 btn_delete_notes = QPushButton('Видалити замітку')
+line_x1.addWidget(btn_delete_notes)
 btn_save_notes = QPushButton('Зберегти замітку')
+line2.addWidget(btn_save_notes)
+
+
+lmt_notes = QLabel('Список тегів ')
+line2.addWidget(lmt_notes)
+
+lpt_notes = QListWidget()
+line2.addWidget(lpt_notes)
+
 btn_add_notes = QPushButton('Додати до замітки')
+line_x2.addWidget(btn_add_notes)
 btn_unpin_notes = QPushButton('Відкріпити замітку')
+line_x2.addWidget(btn_unpin_notes)
 btn_search_notes = QPushButton('Шукати замітку за тегом')
+line2.addWidget(btn_search_notes)
 
 def show_notes():
     key = lst_notes.selectedItems()[0].text()
@@ -47,11 +62,13 @@ def show_notes():
 with open("notes.json", 'r', encoding="utf-8") as file:
     notes = json.load(file)
 
-lst_notes.addItem(notes)
+#lst_notes.addItem(notes)
 
 window.setLayout(layout_notes)
 window.setLayout(line1)
 window.setLayout(line2)
+window.setLayout(line_x1)
+window.setLayout(line_x2)
 
 window.show()
 app.exec_()
