7bfc8dc042387df02c9ca45283f594407de98193
Markian27 <144822485+Markian27@users.noreply.github.com>
2023-10-27 20:03:00 +0300

    Update easy_editor.py

--- logika/m4/easy_editor/easy_editor.py
+++ logika/m4/easy_editor/easy_editor.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFilter
 import os
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QTextEdit, QLabel,
     QListWidget, QPushButton, QLineEdit, QFileDialog, QHBoxLayout, QVBoxLayout, QInputDialog,
@@ -24,8 +25,8 @@ line_x1.addLayout(line_y1, stretch=1)
 line_x1.addLayout(line_y2, stretch=4)
 
 
-folder = QPushButton('Папка')
-line_y1.addWidget(folder)
+btn_folder = QPushButton('Папка')
+line_y1.addWidget(btn_folder)
 left = QPushButton('Вліво')
 line_x2.addWidget(left)
 right = QPushButton('Вправо')
@@ -40,10 +41,6 @@ line_x2.addWidget(BMW)
 List = QListWidget()
 line_y1.addWidget(List)
 
-
-wordir = QFileDialog.getExistingDirectory()
-files_and_folders = os.listdir(wordir)
-
 def filter(files):
     result = []
     ext = ['jpg', 'jpeg', 'bmp', 'gif', 'jfif', 'svg', 'png']
@@ -53,13 +50,55 @@ def filter(files):
             result.append(file)
     return result
 
-print(filter(files_and_folders))
+def showFilter():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    files_and_folders = os.listdir(workdir)
+
+    filtered_img = filter(files_and_folders)
+
+    List.clear()
+    List.addItems(filtered_img)
+
+class ImageProcessor():
+    def __init__(self):
+        self.filename = None
+        self.original = None
+        self.save_dir = 'Modifiled/'
+
+    def loadImage(self, filename):
+        self.filename = filename
+
+        full_path = os.path.join(workdir, filename)
+
+
+        self.original = Image.open(full_path)
+    def showImage(self, path):
+        lb_pic.hide()
+        pixmapimg = QPixmap(path)
+        w, h = lb_pic.width(), lb_pic.height()
+
+        pixmapimg = pixmapimg.scaled(w, h, Qt.KeepAspectRatio)
+
+
+        lb_pic.setPixmap(pixmapimg)
+        lb_pic.show()
+
+workimage = ImageProcessor()
+def showChosenImage():
+    filename = List.currentItem().text()
+    full_path = os.path.join(workdir, filename)
+    workimage.loadImage(filename)
+
 
 window.setLayout(line_x1)
 
+List.itemClicked.connect(showChosenImage)
+btn_folder.clicked.connect(showFilter)
+
 List.setStyleSheet("QListWidget { background-color:"
                      " #f2edc4; color: black; font-size: 16px; border-radius: 1px; }")
-folder.setStyleSheet("QPushButton { background-color:"
+btn_folder.setStyleSheet("QPushButton { background-color:"
                      " #ede393; color: black; font-size: 16px; border-radius: 1px; }")
 BMW.setStyleSheet("QPushButton { background-color:"
                      " #505052; color: white; font-size: 16px; border-radius: 1px; }")
@@ -71,6 +110,7 @@ sharpness.setStyleSheet("QPushButton { background-color:"
                      " #c9c7c7; color: black; font-size: 16px; border-radius: 1px; }")
 mirror.setStyleSheet("QPushButton { background-color:"
                      " #8dd6d5; color: black; font-size: 16px; border-radius: 1px; }")
+
 
 
 window.show()
